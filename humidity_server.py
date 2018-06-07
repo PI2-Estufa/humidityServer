fdc78662e36b4f1b39c0857972155d3db794e0e5
Lucas Amoêdo <lucas.advc@gmail.com>
2018-06-07 16:10:56 -0300

    Remove queues and parse values to int

--- humidity_server.py
+++ humidity_server.py
@@ -1,22 +1,16 @@
 from nameko.rpc import rpc
-from nameko.messaging import Publisher
-from kombu import Exchange, Queue
 import db
 from db import Humidity
-
-exchange = Exchange("main", "direct", durable=True)
-queue = Queue("humidity_queue", exchange=exchange)
 
 
 class HumidityServer():
     name = "humidity_server"
-    publish = Publisher(exchange=exchange, queue=queue)
 
     @rpc
     def receive_humidity(self, humidity):
+        humidity = int(humidity)
         h = Humidity()
         h.value = humidity
         db.session.add(h)
         db.session.commit()
-        self.publish(humidity)
         return humidity
